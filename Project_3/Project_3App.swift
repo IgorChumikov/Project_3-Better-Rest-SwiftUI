270d2921e398ef7876cacbc8eb47f975a6ab3d56
IgorChumikov <chumikov.igor@mail.ru>
2022-02-23 16:27:41 +0300

    Initial Commit

---
+++ Project_3/Project_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3App.swift
+//  Project_3
+//
+//  Created by Игорь Чумиков on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
